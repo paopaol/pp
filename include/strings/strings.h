c00d623b063211c12e5a2980be61fff74edde993
paopaol <467847281@qq.com>
2019-01-10 09:30:43 +0800

    format code

--- include/strings/strings.h
+++ include/strings/strings.h
@@ -4,17 +4,17 @@
 #include <string>
 #include <vector>
 
-
-
-namespace pp{
-	namespace strings{
-		std::string TrimSpace(const std::string &str);	
-		std::string ToUpper(const std::string &str);
-		std::string ToLower(const std::string &str);
-		std::vector<std::string> &Split(std::vector<std::string> &vstr, const std::string &str, const std::string &pattern);
-		bool Contains(const std::string &s, const std::string &substr);
-		std::string Join(const std::vector<std::string> &a, const std::string &sep);
-	}
-}
+namespace pp {
+namespace strings {
+    std::string               TrimSpace(const std::string& str);
+    std::string               ToUpper(const std::string& str);
+    std::string               ToLower(const std::string& str);
+    std::vector<std::string>& Split(std::vector<std::string>& vstr,
+                                    const std::string&        str,
+                                    const std::string&        pattern);
+    bool        Contains(const std::string& s, const std::string& substr);
+    std::string Join(const std::vector<std::string>& a, const std::string& sep);
+}  // namespace strings
+}  // namespace pp
 
 #endif
--- src/strings/strings.cpp
+++ src/strings/strings.cpp
@@ -1,130 +1,129 @@
-#include <strings.h>
+#include <strings/strings.h>
 
-#include <functional>
 #include <algorithm>
+#include <ctype.h>
+#include <functional>
 #include <vector>
-#include<ctype.h>
 
-namespace pp{
-	using namespace std;
-	namespace strings{
-		bool isSpace(unsigned char c)
-		{
-			return (bool)isspace((unsigned char)c);
-		}
+namespace pp {
+using namespace std;
+namespace strings {
+    bool isSpace(unsigned char c)
+    {
+        return ( bool )isspace(( unsigned char )c);
+    }
 
+    string TrimRightFunc(const std::string s, std::function<bool(char)> f)
+    {
+        for (auto i = s.size() - 1; i > 0; i--) {
+            unsigned char c = s[i];
+            if (f(c) == false) {
+                string str(s.data(), i + 1);
+                return str;
+            }
+        }
 
-		string TrimRightFunc(const std::string s, std::function<bool (char)> f)
-		{
-			for (auto i = s.size() - 1; i > 0; i--){
-				unsigned char c = s[i];
-				if (f(c) == false){
-					string	str(s.data(), i + 1);
-					return str;
-				}
-			}
+        return "";
+    }
 
-			return "";
-		}
+    string TrimLeftFunc(const std::string s, std::function<bool(char)> f)
+    {
+        for (unsigned i = 0; i < s.size(); i++) {
+            if (f(s[i]) == false) {
+                string str(s.data() + i, s.size() - i);
+                return str;
+            }
+        }
 
-		string TrimLeftFunc(const std::string s, std::function<bool (char)> f)
-		{
-			for (unsigned i = 0; i < s.size(); i++){
-				if (f(s[i]) == false){
-					string	str(s.data() + i, s.size() - i);
-					return str;
-				}
-			}
+        return "";
+    }
 
-			return "";
-		}
+    string TrimRight(const std::string s, const std::string trim)
+    {
+        if (s == "") {
+            return s;
+        }
+        return TrimRightFunc(s, isSpace);
+    }
 
+    string TrimLeft(const std::string s, const std::string trim)
+    {
+        if (s == "") {
+            return s;
+        }
+        return TrimLeftFunc(s, isSpace);
+    }
 
-		string TrimRight(const std::string s, const std::string trim)
-		{
-				if (s == ""){
-					return s;
-				}
-				return TrimRightFunc(s, isSpace);
-		}
+    string TrimSpace(const string& str)
+    {
+        if (str == "") {
+            return 0;
+        }
+        return TrimLeft(TrimRight(str, ""), "");
+    }
 
-		string TrimLeft(const std::string s, const std::string trim)
-		{
-			if (s == ""){
-				return s;
-			}
-			return TrimLeftFunc(s, isSpace);
-		}
+    string ToUpper(const string& str)
+    {
+        string upperString(str);
 
+        transform(upperString.begin(), upperString.end(), upperString.begin(),
+                  ::toupper);
+        return upperString;
+    }
 
+    string ToLower(const string& str)
+    {
+        string lowerString(str);
 
-		string TrimSpace(const string &str)
-		{
-			if (str == ""){
-				return 0;
-			}
-			return TrimLeft(TrimRight(str, ""), "");
-		}
+        transform(lowerString.begin(), lowerString.end(), lowerString.begin(),
+                  ::tolower);
+        return lowerString;
+    }
 
-		string ToUpper(const string &str)
-		{
-			string	upperString(str);
+    vector<string>& Split(vector<string>& vstr, const string& str,
+                          const string& pattern)
+    {
+        vstr.resize(0);
 
-			transform(upperString.begin(), upperString.end(), upperString.begin(), ::toupper);
-			return upperString;
-		}
+        if (str == "" || pattern == "") {
+            return vstr;
+        }
 
-		string ToLower(const string &str)
-		{
-			string lowerString(str);
+        string s    = str + pattern;
+        size_t size = s.size();
 
-			transform(lowerString.begin(), lowerString.end(), lowerString.begin(), ::tolower);
-			return lowerString;
-		}
+        for (size_t pos = s.find(pattern); pos != string::npos;) {
+            string x(s.substr(0, pos));
+            vstr.push_back(x);
+            s   = s.substr(pos + 1, size);
+            pos = s.find(pattern);
+        }
+        return vstr;
+    }
 
-		vector<string> &Split(vector<string> &vstr, const string &str, const string &pattern)
-		{
-			vstr.resize(0);
+    bool Contains(const string& s, const string& substr)
+    {
+        if (s.find(substr) != string::npos) {
+            return true;
+        }
+        return false;
+    }
 
-			if (str == "" || pattern == ""){
-				return vstr;
-			}
+    string Join(const vector<string>& a, const string& sep)
+    {
+        if (a.size() == 0) {
+            return "";
+        }
+        if (a.size() == 1) {
+            return a[0];
+        }
 
-			string s = str + pattern;
-			size_t size = s.size();
-
-			for (size_t pos = s.find(pattern); pos != string::npos;){
-				string x(s.substr(0, pos));
-				vstr.push_back(x);
-				s = s.substr(pos + 1, size);
-				pos = s.find(pattern);
-			}
-			return vstr;
-		}
-
-		bool Contains(const string &s, const string &substr)
-		{
-			if (s.find(substr) != string::npos){
-				return true;
-			}
-			return false;
-		}
-
-		string Join(const vector<string> &a, const string &sep)
-		{
-			if (a.size() == 0){
-				return "";
-			}
-			if (a.size() == 1){
-				return a[0];
-			}
-			
-			string b = *(a.begin());
-			for (auto it = a.begin() + 1; it != a.end(); it++){
-				b.append(sep);
-				b.append(*it);
-			}
-			return b;
-		}
-	}
-}
+        string b = *(a.begin());
+        for (auto it = a.begin() + 1; it != a.end(); it++) {
+            b.append(sep);
+            b.append(*it);
+        }
+        return b;
+    }
+}  // namespace strings
+}  // namespace pp
