2b9e10c8fa15ea28d4e25e4c5532a47ded6d5620
paopaol <467847281@qq.com>
2019-11-02 23:10:37 +0800

    thread local use c++11 support

--- src/system/windows/sys_win_thread_local_storage.h
+++
@@ -1,9 +0,0 @@
-#ifndef THREAD_LOCAL_STORAGE_H
-#define THREAD_LOCAL_STORAGE_H
-
-extern void set_current_thread_loop(void *loop);
-extern void clear_current_thread_loop();
-extern void *loop_curren_thread_loop();
-extern void thread_local_storage_init();
-
-#endif // THREAD_LOCAL_STORAGE_H
